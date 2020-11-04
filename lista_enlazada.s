a0570b2293cee8132724ed6b4333f9aa38f7f4df
GabrielEberlein <gjeberlein@gmail.com>
2020-11-03 23:35:57 -0300

    Creamos el menu y la funcion para leer la lista

--- lista_enlazada.s
+++ lista_enlazada.s
@@ -1,17 +1,36 @@
 #Declaramos macros
 
 .data 
+        opcionesMenu: .asciiz "Para ver la lista presione 0, para ingresas un nuevo nodo \n1, para eliminar un nodo 2 o 3 para finalizar el programa: " 
+        OpcionDos: .asciiz "Ingrese el nodo que quiere eliminar: "
+        opcionUno: .asciiz "Ingrese los datos del nuevo nodo: "
+        opcionZero: .asciiz "La lista es: "
+        empty: .asciiz "La lista no tiene elementos\n"
         nodo: .word 0
 .text
 
 main:
+        la $a0, opcionesMenu    #imprimimos las opciones
+	li $v0, 4
+    	syscall
+
+        li $v0, 5               #leemos la opcion pedida por el menu
+        syscall
+        
+        
+        lw $a0, nodo($0)
+        lw $a1, nodo($0)
+        beq $v0, $zero, mostrarLista
         li $t0, 1
-        li $v0, 1
-        lw $a0, nodo
         beq $v0, $t0, condicionalNuevoNodo
+        li $t0, 2
+        #beq $v0, $t0, condicionalEliminarNodo
+        li $t0, 3
+        beq $v0, $t0, fin
         
 fin:
-        jr $ra
+        li $v0, 10
+        syscall
 
 condicionalNuevoNodo:
         addi $sp, $sp, -4
@@ -19,28 +38,8 @@ condicionalNuevoNodo:
         jal nuevoNodo #Llamamos a nuevoNodo para el primerNodo
         lw $ra, 0($sp)
         addi $sp, $sp, 4
-        
-        li $t0, 1
-        li $v0, 1               
-        lw $a0, nodo
-        
-        addi $sp, $sp, -4
-        sw $ra, 0($sp)
-        jal nuevoNodo #Agrego un segundo nodo pra probar
-        lw $ra, 0($sp)
-        addi $sp, $sp, 4
 
-        li $t0, 1
-        li $v0, 1               
-        lw $a0, nodo
-        
-        addi $sp, $sp, -4
-        sw $ra, 0($sp)
-        jal nuevoNodo #Agrego un tercer nodo pra probar
-        lw $ra, 0($sp)
-        addi $sp, $sp, 4
-
-        j fin 
+        j main
         
 nuevoNodo:#en $a0 recibimos la direccion de un nodo
         move $t0, $a0
@@ -57,26 +56,25 @@ nuevoNodo:#en $a0 recibimos la direccion de un nodo
         
         lw $t2, nodo($0)
         
-        sw $t2, 0($v0)                  #nuevonodo -> ant = nodo
-        lw $t1, 4($t2)
-        sw $t1, 4($v0)                  #nuevonodo -> sig = nodo -> sig  
+        sw $t2, 4($v0)                  #nuevonodo -> sig = nodo
+        lw $t1, 0($t2)
+        sw $t1, 0($v0)                  #nuevonodo -> ant = nodo -> ant  
 
-        sw $v0, 4($t2)                  #nodo -> sig = nuevonodo
+        sw $v0, 0($t2)                  #nodo -> ant = nuevonodo
         
-        lw $t2, 4($v0)                  #t2 = nuevonodo -> sig  
-        sw $v0, 0($t2)                  #t2 -> ant = nuevonodo
+        lw $t2, 0($v0)                  #t2 = nuevonodo -> ant  
+        sw $v0, 4($t2)                  #t2 -> sig = nuevonodo
 
         move $t2, $v0
 
         li $a0, 50                      #pedimos 50 de espacio para el string
-        addi $sp, $sp, -4 
+        addi $sp, $sp, -4
         sw $ra, 0($sp)
         jal malloc                      #salto a malloc
         lw $ra, 0($sp)
         addi $sp, $sp, 4
         
         sw $v0, 8($t2)                  #nuevonodo -> dato 0 string*
-        sw $t2, nodo($0)
 
         move $a0, $v0                   #paso los argumentos a leerNodo
         li $a1, 50
@@ -119,7 +117,36 @@ primerNodo:
 
         jr $ra
 
-malloc: li $v0, 9       
+
+mostrarLista:
+      
+        beq $a1, $zero, listaVacia
+
+        lw $a0, 8($a1)          
+	li $v0, 4
+	syscall
+
+        lw $t0, 4($a1)
+        move $a1, $t0
+        lw $a0, nodo($0)
+        bne $a1, $a0, mostrarLista
+
+        j main
+        
+
+#mostrarPrimerNodo:
+        #la $a0, 8($a0)
+	#li $v0, 4
+	#syscall
+
+listaVacia:
+        la $a0, empty    #decimos que la lista esta vacia 
+	li $v0, 4
+    	syscall
+
+        j main
+
+malloc: li $v0, 9       # espera en $a0 la cantidada de bytes
         syscall # devuelve v0 con el puntero a la memoria pedida
         jr $ra 
 
